e0af0895384d43a8f6232f84b51889736960f1f5
Scylla <luckscylla2@gmail.com>
2018-01-08 18:58:21 +0000

    newly created

---
+++ app.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
+var mongoose = require("mongoose");
+
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+mongoose.connect("mongodb://localhost/blogs", {useMongoClient: true});
+
+// setup database schema
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+app.get('/', function (req, res) {
+    res.render("landing");
+});
+
+// INDEX - Display a list of all blogs
+app.get('/blogs', function (req, res) {
+    Blog.find({}, function(err, blogs){
+        if(err) {
+            console.log("Something wrong !");
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+// NEW - Show a form to make new blog
+app.get('/blogs/new', function (req, res) {
+    res.render("new");
+});
+
+// CREATE - Add a new blog to DB
+app.post('/blogs', function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, blog) {
+        if(err) {
+            res.render("/new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SHOW - Show more info about one blog
+app.get('/blogs/:id', function (req, res) {
+    Blog.findById(req.params.id, function(err, blog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: blog});
+        }
+    });
+});
+
+// EDIT - Show edit form
+app.get('/blogs/:id/edit', function (req, res) {
+    Blog.findById(req.params.id, function(err, blog){
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: blog});
+        }
+    });
+});
+
+// UPDATE - Update one blog to DB
+app.put('/blogs/:id', function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, blog) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+// DELETE - Delete one blog
+app.delete('/blogs/:id', function (req, res) {
+    Blog.findByIdAndRemove(req.params.id, req.body.blog, function(err) {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Blog server has started !");
+});
